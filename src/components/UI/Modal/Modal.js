60da16218901773501e08b9de1035247c9615b0c
Vietanh <vietanhcao1994@gmail.com>
2020-02-26 17:23:28 +0700

    add modal and orderSummary

---
+++ src/components/UI/Modal/Modal.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Modal.css'
+
+const Modal = (props) => {
+  return (
+    <div className='modal'>
+      { props.children }
+    </div>
+  )
+}
+
+export default Modal; 
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,6 +1,8 @@
 import React, { Component, Fragment } from 'react'
 import Burger from '../../components/Layout/Burger/Burger'
 import BuildControls from '../../components/Layout/Burger/BuildControls/BuildControls';
+import Modal from '../../components/UI/Modal/Modal';
+import OrderSummary from '../../components/Layout/Burger/OrderSummary/OrderSummary';
 
 
 const INGREDIENT_PRICES = {
@@ -73,6 +75,9 @@ export default class BurgerBuilder extends Component {
     }
     return (
       <Fragment>
+        <Modal >
+          <OrderSummary ingredients={this.state.ingredients}/>
+        </Modal>
         <Burger ingredients={this.state.ingredients} />
         <BuildControls 
           addIngredientHandler={this.addIngredientHandler} 
